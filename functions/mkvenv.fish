4e8e4f3b5afb424fb460d5319a8ed91379ad417d
mattmc3 <mattmc3@gmail.com>
2019-11-21 14:29:04 -0500

    Fix formatting

--- functions/mkvenv.fish
+++ functions/mkvenv.fish
@@ -1,5 +1,5 @@
 function mkvenv -d 'Create a new Python venv'
-	# put all venvs in $VENV_HOME
-	set argv[-1] "$VENV_HOME"/"$argv[-1]"
-	python3 -m venv $argv
+    # put all venvs in $VENV_HOME
+    set argv[-1] "$VENV_HOME"/"$argv[-1]"
+    python3 -m venv $argv
 end
